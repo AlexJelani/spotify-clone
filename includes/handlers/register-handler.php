f023fb50aac6c82eab2e012d7e50bbfc59fdb197
Jelani Alexander <jelanialexander82@gmail.com>
2022-08-28 07:56:11 +0900

    second commit validation inputs

---
+++ includes/handlers/register-handler.php
@@ -0,0 +1,45 @@
+<?php
+
+function sanitizeFormPassword($inputText) {
+    $inputText = strip_tags($inputText);
+    return $inputText;
+}
+function sanitizeFormUsername($inputText) {
+    $inputText = strip_tags($inputText);
+    $inputText = str_replace(" ", "",$inputText);
+    return $inputText;
+}
+function sanitizeFormString($inputText) {
+    $inputText = strip_tags($inputText);
+    $inputText = str_replace(" ", "",$inputText);
+    $firstname = ucfirst(strtolower($inputText));
+    return $inputText;
+}
+
+if(isset($_POST['loginButton'])) {
+    //login button was pressed
+    
+}
+
+
+
+
+
+if(isset($_POST['registerButton'])) {
+    //register button was pressed
+    $username = sanitizeFormUsername($_POST['username']);
+    $firstName = sanitizeFormString($_POST['firstName']);
+    $lastName = sanitizeFormString($_POST['lastName']);
+    $email = sanitizeFormString($_POST['email']);
+    $email2 = sanitizeFormString($_POST['email2']);
+    $password = sanitizeFormPassword($_POST['password']);
+    $password2 = sanitizeFormPassword($_POST['password2']);
+
+    $account->register($username, $firstName, $lastName, $email, $email2, $password, $password2);
+
+   
+    
+}
+
+
+?>
